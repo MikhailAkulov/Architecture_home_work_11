e51c71d4051444dc95fb69743e8fb18a40c7321b
Akuloff.M <Akuloff.M@gmail.com>
2023-01-24 01:25:06 +0300

    Update HW11 project

--- ClinicService/ClinicDesktop/MainForm.Designer.cs
+++ ClinicService/ClinicDesktop/MainForm.Designer.cs
@@ -50,6 +50,17 @@
             this.columnHeaderConsultationDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderDescription = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.buttonLoadConsultations = new System.Windows.Forms.Button();
+            this.buttonShowClient = new System.Windows.Forms.Button();
+            this.buttonUpdateClient = new System.Windows.Forms.Button();
+            this.buttonDeleteClient = new System.Windows.Forms.Button();
+            this.buttonCreatePet = new System.Windows.Forms.Button();
+            this.buttonShowPet = new System.Windows.Forms.Button();
+            this.buttonUpdatePet = new System.Windows.Forms.Button();
+            this.buttonDeletePet = new System.Windows.Forms.Button();
+            this.buttonCreateConsultation = new System.Windows.Forms.Button();
+            this.buttonShowConsultation = new System.Windows.Forms.Button();
+            this.buttonUpdateConsultation = new System.Windows.Forms.Button();
+            this.buttonDeleteConsultation = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listViewClients
@@ -67,10 +78,10 @@
             this.listViewClients.FullRowSelect = true;
             this.listViewClients.GridLines = true;
             this.listViewClients.HideSelection = false;
-            this.listViewClients.Location = new System.Drawing.Point(12, 11);
+            this.listViewClients.Location = new System.Drawing.Point(12, 12);
             this.listViewClients.MultiSelect = false;
             this.listViewClients.Name = "listViewClients";
-            this.listViewClients.Size = new System.Drawing.Size(793, 212);
+            this.listViewClients.Size = new System.Drawing.Size(786, 240);
             this.listViewClients.TabIndex = 0;
             this.listViewClients.UseCompatibleStateImageBehavior = false;
             this.listViewClients.View = System.Windows.Forms.View.Details;
@@ -107,10 +118,10 @@
             // 
             // buttonLoadClients
             // 
-            this.buttonLoadClients.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonLoadClients.Location = new System.Drawing.Point(618, 230);
+            this.buttonLoadClients.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonLoadClients.Location = new System.Drawing.Point(12, 258);
             this.buttonLoadClients.Name = "buttonLoadClients";
-            this.buttonLoadClients.Size = new System.Drawing.Size(188, 44);
+            this.buttonLoadClients.Size = new System.Drawing.Size(130, 44);
             this.buttonLoadClients.TabIndex = 1;
             this.buttonLoadClients.Text = "Загрузить список клиентов";
             this.buttonLoadClients.UseVisualStyleBackColor = true;
@@ -118,11 +129,11 @@
             // 
             // buttonCreateClient
             // 
-            this.buttonCreateClient.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonCreateClient.Location = new System.Drawing.Point(423, 230);
-            this.buttonCreateClient.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonCreateClient.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonCreateClient.Location = new System.Drawing.Point(149, 259);
+            this.buttonCreateClient.Margin = new System.Windows.Forms.Padding(4);
             this.buttonCreateClient.Name = "buttonCreateClient";
-            this.buttonCreateClient.Size = new System.Drawing.Size(188, 42);
+            this.buttonCreateClient.Size = new System.Drawing.Size(130, 44);
             this.buttonCreateClient.TabIndex = 2;
             this.buttonCreateClient.Text = "Создать нового клиента";
             this.buttonCreateClient.UseVisualStyleBackColor = true;
@@ -141,10 +152,10 @@
             this.listViewPets.FullRowSelect = true;
             this.listViewPets.GridLines = true;
             this.listViewPets.HideSelection = false;
-            this.listViewPets.Location = new System.Drawing.Point(12, 279);
+            this.listViewPets.Location = new System.Drawing.Point(12, 310);
             this.listViewPets.MultiSelect = false;
             this.listViewPets.Name = "listViewPets";
-            this.listViewPets.Size = new System.Drawing.Size(794, 218);
+            this.listViewPets.Size = new System.Drawing.Size(786, 241);
             this.listViewPets.TabIndex = 3;
             this.listViewPets.UseCompatibleStateImageBehavior = false;
             this.listViewPets.View = System.Windows.Forms.View.Details;
@@ -171,10 +182,10 @@
             // 
             // buttonLoadPets
             // 
-            this.buttonLoadPets.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonLoadPets.Location = new System.Drawing.Point(583, 502);
+            this.buttonLoadPets.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonLoadPets.Location = new System.Drawing.Point(12, 557);
             this.buttonLoadPets.Name = "buttonLoadPets";
-            this.buttonLoadPets.Size = new System.Drawing.Size(222, 43);
+            this.buttonLoadPets.Size = new System.Drawing.Size(130, 44);
             this.buttonLoadPets.TabIndex = 4;
             this.buttonLoadPets.Text = "Загрузить список питомцев";
             this.buttonLoadPets.UseVisualStyleBackColor = true;
@@ -194,10 +205,10 @@
             this.listViewConsultations.FullRowSelect = true;
             this.listViewConsultations.GridLines = true;
             this.listViewConsultations.HideSelection = false;
-            this.listViewConsultations.Location = new System.Drawing.Point(12, 551);
+            this.listViewConsultations.Location = new System.Drawing.Point(12, 607);
             this.listViewConsultations.MultiSelect = false;
             this.listViewConsultations.Name = "listViewConsultations";
-            this.listViewConsultations.Size = new System.Drawing.Size(794, 217);
+            this.listViewConsultations.Size = new System.Drawing.Size(788, 241);
             this.listViewConsultations.TabIndex = 5;
             this.listViewConsultations.UseCompatibleStateImageBehavior = false;
             this.listViewConsultations.View = System.Windows.Forms.View.Details;
@@ -229,21 +240,142 @@
             // 
             // buttonLoadConsultations
             // 
-            this.buttonLoadConsultations.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonLoadConsultations.Location = new System.Drawing.Point(583, 774);
+            this.buttonLoadConsultations.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonLoadConsultations.Location = new System.Drawing.Point(12, 854);
             this.buttonLoadConsultations.Name = "buttonLoadConsultations";
-            this.buttonLoadConsultations.Size = new System.Drawing.Size(222, 42);
+            this.buttonLoadConsultations.Size = new System.Drawing.Size(130, 42);
             this.buttonLoadConsultations.TabIndex = 6;
             this.buttonLoadConsultations.Text = "Загрузить список консультаций";
             this.buttonLoadConsultations.UseVisualStyleBackColor = true;
             this.buttonLoadConsultations.Click += new System.EventHandler(this.buttonLoadConsultations_Click);
+            // 
+            // buttonShowClient
+            // 
+            this.buttonShowClient.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonShowClient.Location = new System.Drawing.Point(286, 258);
+            this.buttonShowClient.Name = "buttonShowClient";
+            this.buttonShowClient.Size = new System.Drawing.Size(130, 44);
+            this.buttonShowClient.TabIndex = 7;
+            this.buttonShowClient.Text = "Показать клиента по Id";
+            this.buttonShowClient.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdateClient
+            // 
+            this.buttonUpdateClient.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonUpdateClient.Location = new System.Drawing.Point(422, 258);
+            this.buttonUpdateClient.Name = "buttonUpdateClient";
+            this.buttonUpdateClient.Size = new System.Drawing.Size(130, 44);
+            this.buttonUpdateClient.TabIndex = 8;
+            this.buttonUpdateClient.Text = "Редактировать запись клиента";
+            this.buttonUpdateClient.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeleteClient
+            // 
+            this.buttonDeleteClient.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonDeleteClient.Location = new System.Drawing.Point(558, 258);
+            this.buttonDeleteClient.Name = "buttonDeleteClient";
+            this.buttonDeleteClient.Size = new System.Drawing.Size(130, 44);
+            this.buttonDeleteClient.TabIndex = 9;
+            this.buttonDeleteClient.Text = "Удалить клиента";
+            this.buttonDeleteClient.UseVisualStyleBackColor = true;
+            // 
+            // buttonCreatePet
+            // 
+            this.buttonCreatePet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonCreatePet.Location = new System.Drawing.Point(149, 557);
+            this.buttonCreatePet.Name = "buttonCreatePet";
+            this.buttonCreatePet.Size = new System.Drawing.Size(130, 44);
+            this.buttonCreatePet.TabIndex = 10;
+            this.buttonCreatePet.Text = "Создать нового питомца";
+            this.buttonCreatePet.UseVisualStyleBackColor = true;
+            // 
+            // buttonShowPet
+            // 
+            this.buttonShowPet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonShowPet.Location = new System.Drawing.Point(286, 557);
+            this.buttonShowPet.Name = "buttonShowPet";
+            this.buttonShowPet.Size = new System.Drawing.Size(130, 44);
+            this.buttonShowPet.TabIndex = 11;
+            this.buttonShowPet.Text = "Показать питомца по Id";
+            this.buttonShowPet.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdatePet
+            // 
+            this.buttonUpdatePet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonUpdatePet.Location = new System.Drawing.Point(422, 557);
+            this.buttonUpdatePet.Name = "buttonUpdatePet";
+            this.buttonUpdatePet.Size = new System.Drawing.Size(130, 44);
+            this.buttonUpdatePet.TabIndex = 12;
+            this.buttonUpdatePet.Text = "Редактировать запись питомца";
+            this.buttonUpdatePet.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeletePet
+            // 
+            this.buttonDeletePet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonDeletePet.Location = new System.Drawing.Point(558, 557);
+            this.buttonDeletePet.Name = "buttonDeletePet";
+            this.buttonDeletePet.Size = new System.Drawing.Size(130, 44);
+            this.buttonDeletePet.TabIndex = 13;
+            this.buttonDeletePet.Text = "Удалить питомца";
+            this.buttonDeletePet.UseVisualStyleBackColor = true;
+            // 
+            // buttonCreateConsultation
+            // 
+            this.buttonCreateConsultation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonCreateConsultation.Location = new System.Drawing.Point(149, 854);
+            this.buttonCreateConsultation.Name = "buttonCreateConsultation";
+            this.buttonCreateConsultation.Size = new System.Drawing.Size(130, 42);
+            this.buttonCreateConsultation.TabIndex = 14;
+            this.buttonCreateConsultation.Text = "Создать новую консультацию";
+            this.buttonCreateConsultation.UseVisualStyleBackColor = true;
+            // 
+            // buttonShowConsultation
+            // 
+            this.buttonShowConsultation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonShowConsultation.Location = new System.Drawing.Point(286, 854);
+            this.buttonShowConsultation.Name = "buttonShowConsultation";
+            this.buttonShowConsultation.Size = new System.Drawing.Size(130, 42);
+            this.buttonShowConsultation.TabIndex = 15;
+            this.buttonShowConsultation.Text = "Показать консультацию по Id";
+            this.buttonShowConsultation.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdateConsultation
+            // 
+            this.buttonUpdateConsultation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonUpdateConsultation.Location = new System.Drawing.Point(422, 854);
+            this.buttonUpdateConsultation.Name = "buttonUpdateConsultation";
+            this.buttonUpdateConsultation.Size = new System.Drawing.Size(130, 42);
+            this.buttonUpdateConsultation.TabIndex = 16;
+            this.buttonUpdateConsultation.Text = "Редактировать консультацию";
+            this.buttonUpdateConsultation.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeleteConsultation
+            // 
+            this.buttonDeleteConsultation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonDeleteConsultation.Location = new System.Drawing.Point(558, 854);
+            this.buttonDeleteConsultation.Name = "buttonDeleteConsultation";
+            this.buttonDeleteConsultation.Size = new System.Drawing.Size(130, 42);
+            this.buttonDeleteConsultation.TabIndex = 17;
+            this.buttonDeleteConsultation.Text = "Удалить консультацию";
+            this.buttonDeleteConsultation.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(817, 946);
+            this.ClientSize = new System.Drawing.Size(809, 901);
+            this.Controls.Add(this.buttonDeleteConsultation);
+            this.Controls.Add(this.buttonUpdateConsultation);
+            this.Controls.Add(this.buttonShowConsultation);
+            this.Controls.Add(this.buttonCreateConsultation);
+            this.Controls.Add(this.buttonDeletePet);
+            this.Controls.Add(this.buttonUpdatePet);
+            this.Controls.Add(this.buttonShowPet);
+            this.Controls.Add(this.buttonCreatePet);
+            this.Controls.Add(this.buttonDeleteClient);
+            this.Controls.Add(this.buttonUpdateClient);
+            this.Controls.Add(this.buttonShowClient);
             this.Controls.Add(this.buttonLoadConsultations);
             this.Controls.Add(this.listViewConsultations);
             this.Controls.Add(this.buttonLoadPets);
@@ -282,6 +414,17 @@
         private System.Windows.Forms.ColumnHeader columnHeaderConsultationDate;
         private System.Windows.Forms.ColumnHeader columnHeaderDescription;
         private System.Windows.Forms.Button buttonLoadConsultations;
+        private System.Windows.Forms.Button buttonShowClient;
+        private System.Windows.Forms.Button buttonUpdateClient;
+        private System.Windows.Forms.Button buttonDeleteClient;
+        private System.Windows.Forms.Button buttonCreatePet;
+        private System.Windows.Forms.Button buttonShowPet;
+        private System.Windows.Forms.Button buttonUpdatePet;
+        private System.Windows.Forms.Button buttonDeletePet;
+        private System.Windows.Forms.Button buttonCreateConsultation;
+        private System.Windows.Forms.Button buttonShowConsultation;
+        private System.Windows.Forms.Button buttonUpdateConsultation;
+        private System.Windows.Forms.Button buttonDeleteConsultation;
     }
 }
 
